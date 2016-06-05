0ab6afa1e1435979af309e4e4e215ef1b51eda86
Wahab Hussain <whussain@csquareonline.com>
2016-06-05 12:44:40 +0500

    Sample Project with a test email Client

---
+++ LateNightCodeWithWahab/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LateNightCodeWithWahab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task taskA = Task.Run(() => Thread.Sleep(2000));
+            Console.WriteLine(taskA.Status);
+            try
+            {
+                taskA.Wait();
+                Console.WriteLine(taskA.Status);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception in taskA.");
+            }
+            Console.Read();
+        }
+    }
+}
